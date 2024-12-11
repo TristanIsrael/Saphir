8fc253247fee3b0a0c32bcf2d74c7c94c648ec76
Tristan Israël <5672131+tibsys@users.noreply.github.com>
2024-12-11 11:15:02 +0100

    Corrections

--- setup/packages/saphir/saphir-av-clamav/av-controller.py
+++ setup/packages/saphir/saphir-av-clamav/av-controller.py
@@ -5,7 +5,7 @@ import threading
 lock = threading.Event()
 
 if __name__ == "__main__":
-    mqtt_client = MqttFactory.create_mqtt_client_domu("av-clamav")
+    #mqtt_client = MqttFactory.create_mqtt_client_domu("av-clamav")
 
     c=ClamAntivirusController()
     c.start()
