4fbab9d3b6f58c65ce49da87eba668206edb0d61
Tristan Israël <5672131+tibsys@users.noreply.github.com>
2024-12-30 22:50:02 +0100

    Travaux

--- setup/packages/saphir/scripts/constants.sh
+++ setup/packages/saphir/scripts/constants.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 REMOTE_USER=admin
-REMOTE_IP=192.168.64.61
+#REMOTE_IP=192.168.64.61
+REMOTE_IP=192.168.64.65
 REMOTE_SRC_DIR=/home/admin/src/
 REMOTE_PACKAGES_DIR=/home/admin/packages/saphir
