80fb94b9d61996dbe89386b3ccb0d9ddbbeb7c49
Tristan Israël <5672131+tibsys@users.noreply.github.com>
2025-05-22 16:03:48 +0200

    Corrections

--- setup/packages/saphir/saphir-av-eset/scan-file.sh
+++ setup/packages/saphir/saphir-av-eset/scan-file.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-lxc-attach -n saphir-container-eset -- /opt/eset/eea/bin/odscan -s --profile="@In-depth scan" --show-scan-info $1
+lxc-attach -n saphir-container-eset -- /opt/eset/eea/bin/odscan -s --profile="@In-depth scan" --show-scan-info "$1"
