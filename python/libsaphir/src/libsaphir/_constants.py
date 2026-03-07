7b01447db94fc98f101a5d77ebc53198e6588511
Tristan Israël <5672131+TristanIsrael@users.noreply.github.com>
2026-03-07 16:23:39 +0100

    fixes

--- python/libsaphir/src/libsaphir/_constants.py
+++ python/libsaphir/src/libsaphir/_constants.py
@@ -1,6 +1,6 @@
 import os
 
-DEVMODE = os.getenv("DEVMODE", False)
+DEVMODE = os.getenv("DEVMODE", "") != ""
 print(f"DEVMODE is {DEVMODE}")
 TOPIC_ANALYSIS = "saphir/analysis"
 TOPIC_ERROR = "saphir/error"
