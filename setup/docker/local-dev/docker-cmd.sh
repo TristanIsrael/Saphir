357f143c8a0764e5c31b1c4b1b2b65193bd5ed9b
Tristan Israël <5672131+TristanIsrael@users.noreply.github.com>
2026-04-03 15:04:51 +0200

    update docker infra

--- setup/docker/local-dev/docker-cmd.sh
+++ setup/docker/local-dev/docker-cmd.sh
@@ -2,7 +2,15 @@
 
 SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
 PRIVATE_KEY="/Volumes/SECURITY/Saphir/abuild/saphir.rsa"
-SAPHIR_SOURCE_PATH="/Users/tristanisrael/Documents/Sources/Saphir"
+if [ -z "$SAPHIR_SOURCE_PATH" ]; then
+    SAPHIR_SOURCE_PATH="/Users/tristanisrael/Documents/Sources/Saphir"
+fi 
+if [ -z "$LOCAL_CACHE" ]; then 
+    LOCAL_CACHE="/Users/tristanisrael/Downloads/abuild_cache"
+fi
+if [ -z "$OUTPUT_REPOSITORY" ]; then 
+    OUTPUT_REPOSITORY="Users/tristanisrael/Downloads/abuild_repo"
+fi
 DOCKER_PATH="/usr/local/bin"
 IMAGE_NAME="saphir-dev"
 DOCKER_NAME="saphir-dev"
@@ -12,18 +20,15 @@ EMULATE=0
 MOUNT_REPO=""
 INTERACTIVE=""
 
-if [ $LOCAL_ARCH != "amd64" ]; then
+if [ $LOCAL_ARCH != "amd64" ] && [ $LOCAL_ARCH != "x86_64" ]; then
     echo "Must emulate x86_64 architecture..."
     EMULATE=1
 fi
 
 if [ -n "$OVERRIDE_REPOSITORIES" ]; then 
     echo "Override repositories in the container"
-    MOUNT_REPO="--mount type=bind,source="$SCRIPT_DIR/repositories",target=/etc/apk/repositories,readonly"
-fi 
-
-if [ "$#" -gt 0 ]; then
-    echo "Adding arguments to docker commande line: $@"
+    MOUNT_REPO="--mount type=bind,source=$SCRIPT_DIR/repositories,target=/etc/apk/repositories,readonly"
+    PIP_REPO="--mount type=bind,source=$SCRIPT_DIR/pip.conf,target=/home/builder/.pip/pip.conf,readonly"
 fi
 
 if [ $EMULATE -eq 1 ]; then
@@ -37,28 +42,35 @@ if [ $EMULATE -eq 1 ]; then
         -it \
         --rm \
         --platform linux/amd64 \
-        --mount type=bind,source="$PRIVATE_KEY",target=/home/builder/.abuild/saphir.rsa,readonly \
         $MOUNT_REPO \
-        -v "$SAPHIR_SOURCE_PATH:/home/builder/src/Saphir" \
+        $PIP_REPO \
+        --mount type=bind,source=$PRIVATE_KEY,target=/home/builder/.abuild/saphir.rsa,readonly \
+        -v "$SAPHIR_SOURCE_PATH:/home/builder/src/saphir" \
+        -v "$LOCAL_CACHE:/var/cache/distfiles" \
+        -v "$OUTPUT_REPOSITORY:/home/builder/packages/saphir" \
         -e STAGE="$STAGE" \
         --name "$DOCKER_NAME" \
+        --network safecor_safecor \
         "$IMAGE_NAME" 
     else 
         echo "Starting docker with a command"
 
         # Run the container
-        echo "$DOCKER_PATH/docker" run \
+        "$DOCKER_PATH/docker" run \
         --rm \
         --platform linux/amd64 \
-        --mount type=bind,source="$PRIVATE_KEY",target=/home/builder/.abuild/saphir.rsa,readonly \
         $MOUNT_REPO \
-        -v "$SAPHIR_SOURCE_PATH:/home/builder/src/Saphir" \
+        $PIP_REPO \
+        --mount type=bind,source=$PRIVATE_KEY,target=/home/builder/.abuild/saphir.rsa,readonly \
+        -v "$SAFECOR_SOURCE_PATH:/home/builder/src/saphir" \
+        -v "$LOCAL_CACHE:/var/cache/distfiles" \
+        -v "$OUTPUT_REPOSITORY:/home/builder/packages/saphir" \
         -e STAGE="$STAGE" \
         --name "$DOCKER_NAME" \
+        --network safecor_safecor \
         "$IMAGE_NAME" \
         sh -c "$@"
     fi
-
 else
     echo "Start native Docker"
 
@@ -69,11 +81,15 @@ else
         "$DOCKER_PATH/docker" run \
         --rm \
         -it \
-        --mount type=bind,source="$PRIVATE_KEY",target=/home/builder/.abuild/saphir.rsa,readonly \
         $MOUNT_REPO \
-        -v "$SAPHIR_SOURCE_PATH:/home/builder/src/Saphir" \
+        $PIP_REPO \
+        -v "$PRIVATE_KEY:/home/builder/.abuild/saphir.rsa:ro" \
+        -v "$SAPHIR_SOURCE_PATH:/home/builder/src/saphir" \
+        -v "$LOCAL_CACHE:/var/cache/distfiles" \
+        -v "$OUTPUT_REPOSITORY:/home/builder/packages/saphir" \
         -e STAGE="$STAGE" \
         --name "$DOCKER_NAME" \
+        --network safecor_safecor \
         "$IMAGE_NAME" 
     else 
         echo "Starting docker with a command"
@@ -81,11 +97,15 @@ else
         # Run the container
         "$DOCKER_PATH/docker" run \
         --rm \
-        --mount type=bind,source="$PRIVATE_KEY",target=/home/builder/.abuild/saphir.rsa,readonly \
         $MOUNT_REPO \
-        -v "$SAPHIR_SOURCE_PATH:/home/builder/src/Saphir" \
+        $PIP_REPO \
+        --mount type=bind,source=$PRIVATE_KEY,target=/home/builder/.abuild/saphir.rsa,readonly \
+        -v "$SAPHIR_SOURCE_PATH:/home/builder/src/saphir" \
+        -v "$LOCAL_CACHE:/var/cache/distfiles" \
+        -v "$OUTPUT_REPOSITORY:/home/builder/packages/safesaphiror" \
         -e STAGE="$STAGE" \
         --name "$DOCKER_NAME" \
+        --network safecor_safecor \
         "$IMAGE_NAME" \
         sh -c "$@"
     fi
